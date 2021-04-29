f1fb89c7eb197af2df3d77642393409572919670
PulakRaj <60375228+PulakRaj@users.noreply.github.com>
2021-04-28 21:54:29 -0400

    Add files via upload

--- wildfire_app_github/index.py
+++ wildfire_app_github/index.py
@@ -5,12 +5,15 @@ import dash_bootstrap_components as dbc
 
 from app import app
 
-from apps import economic_loss, usa
+from apps import economic_loss, usa, basic_ml, advanced_ml
+
 
 dropdown = dbc.DropdownMenu(
     children=[
+        dbc.DropdownMenuItem("Map", href="/usa"),
         dbc.DropdownMenuItem("Economic Impact", href="/economic_loss"),
-        dbc.DropdownMenuItem("USA", href="/usa"),
+        dbc.DropdownMenuItem("Basic Fire Cause Prediction", href="/basic_ml"),
+        dbc.DropdownMenuItem("Advanced Fire Cause Evaluation", href="/advanced_ml")
     ],
     nav = True,
     in_navbar = True,
@@ -29,7 +32,7 @@ navbar = dbc.Navbar(
                     align = "center",
                     no_gutters = True,
                 ),
-                href = "/economic_loss",
+                href = "/usa",
             ),
             dbc.NavbarToggler(id = "navbar-toggler2"),
             dbc.Collapse(
@@ -68,10 +71,14 @@ app.layout = html.Div([
     Output('page-content', 'children'),
     [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/usa':
-        return usa.layout
-    else:
+    if pathname == '/economic_loss':
         return economic_loss.layout
+    elif pathname == '/basic_ml':
+        return basic_ml.layout
+    elif pathname == '/advanced_ml':
+        return advanced_ml.layout
+    else:
+        return usa.layout
 
 if __name__ == '__main__':
     app.run_server(host='127.0.0.1', debug=True)
